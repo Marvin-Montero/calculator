c2835fde8f9a18ae0d4a3ddce3cead9d8707269b
Marvin-Montero <60294914+Marvin-Montero@users.noreply.github.com>
2020-06-29 19:29:41 +0200

    Update Calculator.go

--- calculator.go
+++ calculator.go
@@ -1,45 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+var clear map[string]func()
+
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux if its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows if its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+}
+
+func CallClear() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
 
 func main() {
 
-  opc := 0
+	opc := 0
 
-  var num1, num2, result int
+	var num1, num2, result int
 
-  for{
-      fmt.Println("Select one option from the menu:")
-      fmt.Println("1-Addition \n2-Subtraction \n3-Multiplication \n4-Division")
-      fmt.Scanf("%d\n", &opc)
+	for {
+		fmt.Println("Select one option from the menu:")
+		fmt.Println("1-Addition \n2-Subtraction \n3-Multiplication \n4-Division \n5-Exit")
+		fmt.Scanf("%d\n", &opc)
 
-      fmt.Println("Type the first number: ")
-      fmt.Scanf("%d\n", &num1)
+		if opc == 5 {
 
-      fmt.Println("Type the second number: ")
-      fmt.Scanf("%d\n", &num2)
+			CallClear()
+			fmt.Println("See ya!") //Nice
+			break
+		}
 
-      if opc == 1{
+		fmt.Println("Type the first number: ")
+		fmt.Scanf("%d\n", &num1)
 
-          result = num1+num2
-          fmt.Printf("The result of the addition is %d\n", result)
+		fmt.Println("Type the second number: ")
+		fmt.Scanf("%d\n", &num2)
 
-      }else if opc == 2{
+		if opc == 1 {
 
-          result = num1-num2
-          fmt.Printf("The result of the subtraction is %d\n", result)
+			result = num1 + num2
+			CallClear()
+			fmt.Printf("The result of the addition is %d\n", result)
 
-      }else if opc == 3{
+		} else if opc == 2 {
 
-          result = num1*num2
-          fmt.Printf("The result of the multiplication is %d\n", result)
+			result = num1 - num2
+			CallClear()
+			fmt.Printf("The result of the subtraction is %d\n", result)
 
-      }else if opc == 4{
+		} else if opc == 3 {
 
-          result = num1/num2
-          fmt.Printf("The result of the division is %d\n", result)
+			result = num1 * num2
+			CallClear()
+			fmt.Printf("The result of the multiplication is %d\n", result)
 
-    }
+		} else if opc == 4 {
 
-  }
+			result = num1 / num2
+			CallClear()
+			fmt.Printf("The result of the division is %d\n", result)
+
+		}
+
+	}
 }
